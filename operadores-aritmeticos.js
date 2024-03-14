99e4253117b5321c31bda5be60d97043d50295f7
elisa carolina de souza <160603661+elisacarol@users.noreply.github.com>
2024-03-14 14:38:55 +0000

    criando o arquivo de operadores aritmeticos

---
+++ operadores-aritmeticos.js
@@ -0,0 +1,18 @@
+const numero1 = 40;/*armazenar as informações numa */ 
+const numero2 = 20;
+
+
+/*pensar se estas variaveis serao mudadas depois...*/
+
+/* para copiar linha Alt+Shift+seta para baixo*/
+/*usar o acento grave  para delimitar texto com variáveis*/
+
+console.log(`Os números das operações são ${numero1} e ${numero2}`)
+console.log(`A soma dos números é ${numero1 + numero2}`);
+console.log(`A subtracao dos números é ${numero1 - numero2}`);
+console.log(`A multiplicacao dos números é ${numero1 * numero2}`);
+console.log(`A divisao dos números é ${numero1 / numero2}`);
+console.log(`A resto dos números é ${numero1 % numero2}`);
+
+/*como fazer o teste manual deste código? tem duas opções: 1 incorporo ele no arquivo HTML ou
+no Node*/
